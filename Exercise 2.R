24a79e0a05b333cce219a6d1a565196f2b9595a8
aadityeshwar <aadityeshwar.10@gmail.com>
2016-08-23 13:32:09 +0530

    Titanic original data set loaded.

--- Exercise 2.R
+++ Exercise 2.R
@@ -1,1 +1,15 @@
 # Assignment 2
+
+#Installing the required packages
+
+install.packages("dplyr")
+suppressMessages( library(dplyr) )
+
+install.packages("tidyr")
+suppressMessages( library(tidyr) )
+
+# Task 0: Loading Data into RStudio
+titanic_org<- read.csv("~/Desktop/Data Science/3 Data Wranging/Exercise 2/Data-Wrangling-Exercise-2-Dealing-with-missing-values/titanic_original.csv")
+
+# #Looking at the structure of the data set
+glimpse(titanic_org)
